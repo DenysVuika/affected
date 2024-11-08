de18862ffeb7cd1fe06dfa9e597e234b083d1b19
Denys Vuika <denys.vuika@gmail.com>
2024-11-08 17:31:04 -0500

    initial cli setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(name = "affected")]
+#[command(about = "A tool to find affected files or projects in a git repository", long_about = None)]
+struct Cli {
+    /// The subcommand to run
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    #[command(subcommand)]
+    List(ListTargets),
+}
+
+#[derive(Subcommand)]
+enum ListTargets {
+    All,
+    Projects,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Commands::List(subcommand) => match subcommand {
+            ListTargets::All => {
+                println!("All targets");
+            }
+            ListTargets::Projects => {
+                println!("Projects");
+            }
+        },
+    }
 }
